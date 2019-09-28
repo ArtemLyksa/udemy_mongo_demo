19b49969dc6e240034def3ac8cb9201eb2df45ef
Artem <artemlyksa@gmail.com>
2019-09-28 13:33:29 +0300

    Added saving and quering to MongoDB

--- index.js
+++ index.js
@@ -7,15 +7,35 @@ mongoose.connect('mongodb://localhost/playground')
 const courseSchema = new mongoose.Schema({
     name: String,
     author: String,
-    tags: [ String ],
+    tags: [String],
     date: { type: Date, default: Date.now },
     isPublished: Boolean
 });
 
 const Course = mongoose.model('Course', courseSchema);
-const course = new Course({
-    name: 'Node.js Course',
-    author: 'Mosh',
-    tags: ['node', 'backend'],
-    isPublished: true
-});
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Angular Course',
+        author: 'Mosh',
+        tags: ['angular', 'frontend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'Mosh', isPublished: true })
+        .limit(10)
+        .sort({ name: 1 }) //1: ascending, -1: descending
+        .select({ name: 1, tags: 1 });
+
+
+    console.log(courses);
+}
+
+getCourses();
+// createCourse();
