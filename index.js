deef196d93049a19ac563a0fe9af2c6365d51622
Artem <artemlyksa@gmail.com>
2019-10-01 18:31:12 +0300

    Added validator for mongodb scheme

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch((error) => console.error('Could not connect to MongoDB...', error));
 
 const courseSchema = new mongoose.Schema({
-    name: String,
+    name: { type: String, required: true },
     author: String,
     tags: [String],
     date: { type: Date, default: Date.now },
@@ -22,61 +22,28 @@ async function createCourse() {
         isPublished: true
     });
 
-    const result = await course.save();
-    console.log(result);
+    try {
+        const result = await course.save();
+        console.log(result);
+    }
+    catch (exception) {
+        console.error(exception.message)
+    }
+
 }
+createCourse();
 
 async function getCourses() {
-    // COMPARISON OPERATORS:
-    // eq (equal)
-    // ne (not equal)
-    // gt (greater than)
-    // gte (greater then or equal to)
-    // lt (less then)
-    // lte (less then or equal to)
-    // in 
-    // nin (not in)
-
-    //EXAMPLES: 
-    // .find({ price: { $gte: 10, $lte: 20 } })
-    // .find({ price: { $in: [10, 15, 20] } })
-
-    // LOGICAL OPERATORS:
-    // or
-    // and
-
-    //EXAMPLES: 
-    // .find()
-    // .or([ { author: 'Mosh' }, { isPublished: true } ])
-    // .and([ { author: 'Mosh' }, { isPublished: true } ])
-
-    //REGULAR EXPRESSIONS:
-    //Starts with Mosh
-    // .find({ author: /^Mosh/ }) ^ - start of the string
-
-    //Starts with Hamedani
-    // .find({ author: /Hamedani$/i }) $ - end of the string, i - case insensitive
-
-    //Contains Mosh
-    // .find({ author: /.*Mosh.*/i }) - .* - zero or more characters.
-
     const courses = await Course
         .find({ author: 'Mosh', isPublished: true })
         .limit(10)
-        .sort({ name: 1 }) //1: ascending, -1: descending
-        .count()
-    // .select({ name: 1, tags: 1 });
+        .sort({ name: 1 }) 
+        .countDocuments()
     console.log(courses);
 }
 
 getCourses();
-// createCourse();
 
-
-// Approach: Query first
-// findById()
-// Modify its properties
-// save()
 async function updateCourse(id) {
     const course = await Course.findById(id);
     if (!course) return;
@@ -85,20 +52,12 @@ async function updateCourse(id) {
     course.author = 'Another author1';
     const result = await course.save();
     console.log(result);
-
-    // course.set({
-    //     isPublished: true,
-    //     author: 'Another author'
-    // });
 }
 // updateCourse('5d8f34e9ce6d060451f47b65');
 
-// Approach: Update first
-// Update directly
-// Optionally: get the updated docu`ment
 async function updateCourse2(id) {
 
-    const result = await Course.update({ _id: id }, {
+    const result = await Course.updateOne({ _id: id }, {
         $set: {
             author: 'Artem',
             isPublished: false
